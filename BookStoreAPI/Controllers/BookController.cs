2b11db58ae7f92c015f516879ad55b2a206b19f0
Sara Awad <sara.awad@ecole-isitech.fr>
2023-12-13 09:21:43 +0100

    commit de mardi

--- BookStoreAPI/Controllers/BookController.cs
+++ BookStoreAPI/Controllers/BookController.cs
@@ -8,6 +8,7 @@ namespace BookStoreAPI.Controllers; //bookstoreAPI est l'espace
 //ici elle permet de définir que la calss BookController est un controller API 
 // la décorateur
 [ApiController]
+[Route("api/[controller]")]//permet de ne plus mettre [HttpPost[books]]
 public class BookController : ControllerBase
 {
     [HttpGet("books")]
@@ -20,6 +21,12 @@ public class BookController : ControllerBase
         };
 
         return Ok(books);
-
     }
+
+    // [HttpPost]
+    // public CreateBook ([FromBody] Book book) //[FromBody] permet de dire ou il faut aller chercher dans le Frombody pour avoir le Book
+    // {
+    //     Console.WriteLine(book.Title);
+    //     return CreatAtAction(nameof(GetBooks), new {id = book.Id}, book);
+    // }
 }
